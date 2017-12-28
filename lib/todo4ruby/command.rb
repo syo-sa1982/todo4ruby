83091b6699f7c7278d4afa6e511b5ca842268ee1
syo-sa1982 <fujii1308@gmail.com>
2017-12-28 23:15:13 +0900

    taskモデルクラス - create,delete,update

--- lib/todo4ruby/command.rb
+++ lib/todo4ruby/command.rb
@@ -4,5 +4,30 @@ module Todo
 
   # コマンドラインベースの処理を行うクラス
   class Command
+
+    def execute
+      DB.prepare
+    end
+
+    def create_task(name, content)
+      Task.create!(name: name, content: content).reload
+    end
+
+    def delete_task(id)
+      task = Task.find(id)
+      task.destroy
+    end
+
+    def update_task(id, attributes)
+      status_name = attributes[:status]
+      if status_name
+        attributes[:status] = Task::STATUS.fetch(status_name.upcase)
+      end
+
+      task = Task.find(id)
+      task.update_attributes! attributes
+
+      task.reload
+    end
   end
 end
